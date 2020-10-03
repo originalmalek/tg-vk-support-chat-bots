f8aa5788587a55e706851073f16fcd8d604a6e07
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-10-03 17:47:09 +0300

    Structured logger, moved detect_intent_text to new file

--- tg_bot.py
+++ tg_bot.py
@@ -1,24 +1,17 @@
-import dialogflow_v2 as dialogflow
-import os
-import telebot
 import logging
+import os
 
-from google.oauth2 import service_account
+import telebot
+
+from detect_intent_text import detect_intent_text
 from dotenv import load_dotenv
+from telegram_logger import MyLogsHandler
 
+logger = logging.getLogger('TG')
+
+load_dotenv()
 telegram_token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(telegram_token)
-
-
-def detect_intent_text(dialogflow_project_id, chat_id, text, language_code):
-    session_client = dialogflow.SessionsClient(credentials=credentials)
-    session = session_client.session_path(dialogflow_project_id, chat_id)
-
-    text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    response = session_client.detect_intent(session=session, query_input=query_input)
-
-    bot.send_message(chat_id=chat_id, text=response.query_result.fulfillment_text)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -26,31 +19,19 @@ def send_welcome(message):
     bot.reply_to(message, "Здравствуйте, чем я могу вам помочь?")
 
 
+def send_answer_to_telegram(chat_id, text, language_code):
+    answer = detect_intent_text(chat_id, text, language_code)
+    bot.send_message(chat_id=chat_id, text=answer.query_result.fulfillment_text)
+
 @bot.message_handler(content_types='text')
 def send_question_to_dialogflow(message):
-    detect_intent_text(dialogflow_project_id, message.chat.id, message.text, 'RU')
-
-
-def send_log_message(telegram_token, telegram_chat_id, text):
-    bot = telebot.TeleBot(telegram_token)
-    bot.send_message(telegram_chat_id, text)
+    send_answer_to_telegram(message.chat.id, message.text, 'RU')
 
 
 if __name__ == '__main__':
-    load_dotenv()
-
-    credentials = service_account.Credentials.from_service_account_file("google-credentials.json")
-    dialogflow_project_id = os.environ['DIALOGFLOW_PROJECT_ID']
-    telegram_token = os.environ['TELEGRAM_TOKEN']
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
-
-    class MyLogsHandler(logging.Handler):
-        def emit(self, record):
-            log_entry = self.format(record)
-            send_log_message(telegram_token, telegram_chat_id, log_entry)
-
+   
     logging.basicConfig(level=10)
-    logger = logging.getLogger('TG')
     logger.addHandler(MyLogsHandler())
 
     while True:
